28882eae63e9dfa6f6518f138547555f16625388
titus buchanan <titusbuchananjr@gmail.com>
2021-03-23 23:34:24 -0400

    intialized all fields in class

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -11,9 +11,13 @@ public class Student {
 
     //Constructor creating new Student objects by initializing values
     public Student(int id, String name, int grade ){
+        this.feespaid = 0;
+        this.feestotal = 30000;
         this.id = id;
         this.name = name;
         this.grade = grade;
     }
+
+
 
 }
